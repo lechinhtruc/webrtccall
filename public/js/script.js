2533c888db7932860ee13a0b5e896d5c9bb5eee6
Lê Trực <34809922+lechinhtruc@users.noreply.github.com>
2022-03-15 09:56:55 +0700

    Update

--- public/js/script.js
+++ public/js/script.js
@@ -24,6 +24,7 @@ navigator.mediaDevices
       call.answer(stream);
       const video = document.createElement("video");
       call.on("stream", (userVideoStream) => {
+	alert(userVideoStream);
         addVideoStream(video, userVideoStream);
       });
     });
