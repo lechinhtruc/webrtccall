970083de4b2c39d0a29459cd3ad08f38a776c890
Lê Trực <34809922+lechinhtruc@users.noreply.github.com>
2022-03-15 08:45:23 +0700

    Update

--- public/js/script.js
+++ public/js/script.js
@@ -1,8 +1,10 @@
 const socket = io("/");
 const videoGrid = document.getElementById("video-grid");
 const myPeer = new Peer(undefined, {
-	host: "/",
-	port: "25522"
+	host: "webrtc3010.herokuapp.com",
+	port: "443", 
+	key : "peerjs",
+	secure : true
 });
 const myVideo = document.createElement("video");
 myVideo.muted = true;
