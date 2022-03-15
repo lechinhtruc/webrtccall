7a4240e06673231d4c1014ad88b7ae8f72731329
Lê Trực <34809922+lechinhtruc@users.noreply.github.com>
2022-03-15 10:04:31 +0700

    Update

--- public/js/script.js
+++ public/js/script.js
@@ -6,7 +6,8 @@ const myPeer = new Peer(undefined, {
 	key : "peerjs",
 	secure : true,
 	config: {'iceServers': [
-    	{ url: 'stun:stun2.l.google.com:19302' }
+    	{ url: 'stun:stun2.l.google.com:19302' },
+	{ url: 'turn:192.158.29.39:3478?transport=udp', credential: 'JZEOEt2V3Qb0y27GRntt2u2PAYA=', username: '28224511:1379330808' }
   ]}
 });
 const myVideo = document.createElement("video");
